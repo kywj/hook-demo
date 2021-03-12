487d1058d88792c9e88e59965d5d6f59b299a591
ky <kywj1997@gmail.com>
2021-03-12 15:23:42 +0800

    bind key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
 const App = () => {
   return (
-    <div>APP 组件</div>
+    <div>APP 组件
+      <Component numbers={['a', 'b', 'c']} />
+    </div>
+  )
+}
+
+const Component = (props) => {
+  return (
+    <div>
+      {props.numbers.map((i, index) => {
+        return <div key={index}>下标是{index}值为{i}</div>
+      })}
+    </div>
   )
 }
 
